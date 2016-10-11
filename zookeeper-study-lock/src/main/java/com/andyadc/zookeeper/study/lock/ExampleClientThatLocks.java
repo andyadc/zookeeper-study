6a4aca4081443ae5488f2e58b61270aecb420186
andaicheng <andaicheng@live.com>
2016-10-11 18:52:20 +0800

    InterProcessMutexExample

---
+++ zookeeper-study-lock/src/main/java/com/andyadc/zookeeper/study/lock/ExampleClientThatLocks.java
@@ -0,0 +1,41 @@
+package com.andyadc.zookeeper.study.lock;
+
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.recipes.locks.InterProcessLock;
+import org.apache.curator.framework.recipes.locks.InterProcessMutex;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author andaicheng
+ */
+public class ExampleClientThatLocks {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExampleClientThatLocks.class);
+
+    private final InterProcessLock lock;
+    private final FakeLimitedResource resource;
+    private final String clientName;
+
+    public ExampleClientThatLocks(CuratorFramework framework, String lockPath, FakeLimitedResource resource, String clientName) {
+        this.resource = resource;
+        this.clientName = clientName;
+        lock = new InterProcessMutex(framework, lockPath);
+    }
+
+    public void doWork(long time, TimeUnit unit) throws Exception {
+        if (!lock.acquire(time, unit)) {
+            LOGGER.error(clientName + " could not acquire the lock");
+            throw new IllegalStateException(clientName + " could not acquire the lock");
+        }
+        try {
+            LOGGER.info(clientName + " has the lock");
+            resource.use(); //access resource exclusively
+        } finally {
+            LOGGER.info(clientName + " releasing the lock");
+            lock.release(); // always release the lock in a finally block
+        }
+    }
+}
---
+++ zookeeper-study-lock/src/main/java/com/andyadc/zookeeper/study/lock/InterProcessMutexExample.java
@@ -0,0 +1,15 @@
+package com.andyadc.zookeeper.study.lock;
+
+/**
+ * @author andaicheng
+ */
+public class InterProcessMutexExample {
+
+    private static final int QTY = 5;
+    private static final int REPETITIONS = QTY * 10;
+    private static final String PATH = "examples_locks";
+
+    public static void main(String[] args) {
+
+    }
+}
