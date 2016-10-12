171d08c51bc806d7b3c9bfcc3c6ae151286d2d49
andaicheng <andaicheng@live.com>
2016-10-12 15:42:19 +0800

    InterProcessMutexExample

--- zookeeper-study-lock/src/main/java/com/andyadc/zookeeper/study/lock/InterProcessMutexExample.java
+++ zookeeper-study-lock/src/main/java/com/andyadc/zookeeper/study/lock/InterProcessMutexExample.java
@@ -1,5 +1,16 @@
 package com.andyadc.zookeeper.study.lock;
 
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.curator.test.TestingServer;
+import org.apache.curator.utils.CloseableUtils;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author andaicheng
  */
@@ -7,9 +18,39 @@ public class InterProcessMutexExample {
 
     private static final int QTY = 5;
     private static final int REPETITIONS = QTY * 10;
-    private static final String PATH = "examples_locks";
+    private static final String PATH = "/examples_locks";
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        final FakeLimitedResource resource = new FakeLimitedResource();
+        ExecutorService service = Executors.newFixedThreadPool(QTY);
+        final TestingServer server = new TestingServer();
+        try {
+            for (int i = 0; i < QTY; ++i) {
+                final int index = 1;
+                Callable<Void> task = new Callable<Void>() {
+                    @Override
+                    public Void call() throws Exception {
+                        CuratorFramework client = CuratorFrameworkFactory.newClient(server.getConnectString(), new ExponentialBackoffRetry(1000, 3));
+                        try {
+                            client.start();
+                            final ExampleClientThatLocks locks = new ExampleClientThatLocks(client, PATH, resource, "Client: " + index);
+                            for (int j = 0; j < REPETITIONS; ++j) {
+                                locks.doWork(10, TimeUnit.SECONDS);
+                            }
+                        } catch (Throwable e) {
+                            e.printStackTrace();
+                        } finally {
+                            CloseableUtils.closeQuietly(client);
+                        }
+                        return null;
+                    }
+                };
+                service.submit(task);
+            }
+            service.shutdown();
+            service.awaitTermination(10, TimeUnit.MINUTES);
+        } finally {
+            CloseableUtils.closeQuietly(server);
+        }
     }
 }
