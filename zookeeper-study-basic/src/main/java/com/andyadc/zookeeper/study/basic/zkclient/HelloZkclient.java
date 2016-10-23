f1f8b7f1eb0e562407b13f224f8141db27a0d01c
andyadc <andaicheng@live.com>
2016-10-23 14:35:31 +0800

    optimize code

--- zookeeper-study-basic/src/main/java/com/andyadc/zookeeper/study/basic/zkclient/HelloZkclient.java
+++ zookeeper-study-basic/src/main/java/com/andyadc/zookeeper/study/basic/zkclient/HelloZkclient.java
@@ -1,6 +1,9 @@
 package com.andyadc.zookeeper.study.basic.zkclient;
 
 import org.I0Itec.zkclient.ZkClient;
+import org.I0Itec.zkclient.serialize.SerializableSerializer;
+
+import java.util.Date;
 
 /**
  * @author andaicheng
@@ -11,10 +14,10 @@ public class HelloZkclient {
     private static final String IP = "139.196.192.166";
 
     public static void main(String[] args) {
-        ZkClient zkClient = new ZkClient(IP + ":2191," + IP + ":2192," + IP + ":2193");
-        String node = "/zkc";
+        ZkClient zkClient = new ZkClient(IP + ":2191," + IP + ":2192," + IP + ":2193", 15000, 5000, new SerializableSerializer());
+        String node = "/zkc1";
         if (!zkClient.exists(node)) {
-            zkClient.createPersistent(node, "hello zkclient");
+            zkClient.createPersistent(node, new Date());
         }
         Object ret = zkClient.readData(node);
         System.out.println(ret);
